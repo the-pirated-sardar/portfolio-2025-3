89dacbb07e9aa89e4e761d3e6b811b5eb50f4103
Prateek Singh Arora <psarora@sfu.ca>
2025-01-30 12:38:01 -0800

    Set Remix server to Node.js

--- remix.config.js
+++ remix.config.js
@@ -2,8 +2,8 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-    serverBuildTarget: "node-cjs", // Tells Remix to stop using Cloudflare build settings
-    server: "./server.js",
+    serverBuildTarget: "node-cjs",
+    server: "@remix-run/node",
     ignoredRouteFiles: ["**/.*"],
   };
   
